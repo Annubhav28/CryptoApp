b592983cec35e6b0273e1b5d470e3ed443776cab
Anubhav28 <anubhavtrivedi222@gmail.com>
2024-04-10 18:52:36 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,93 @@
+const URL = "https://api.coinlore.net/api/tickers/?start=0&limit=10";
+
+
+
+
+const changebit = document.querySelector(".btc-chng");
+const changebnb = document.querySelector(".bnb-chng");
+const changeeth = document.querySelector(".eth-chng");
+const changeusdt = document.querySelector(".usdt-chng");
+const pricebit = document.querySelector(".bitc");
+const priceeth = document.querySelector(".eth");
+const pricebnb = document.querySelector(".bnb");
+const priceusdt = document.querySelector(".usdt");
+
+const getPrice = async () => {
+    let promise = await fetch(URL);
+    let response = await promise.json();
+
+    const data = response.data
+
+
+    data.forEach((j) => {
+
+        const price = j.price_usd;
+        const market = Number(j.market_cap_usd).toFixed();
+        const change = Number(j.percent_change_24h);
+        const coin = j.name;
+        const symb = j.symbol.toLowerCase();
+        const colorchange = change < 0 ? "#ea3943" : "#16c784" || 'white'; 
+       
+
+
+
+
+
+
+        if (symb === 'btc') {
+
+            changebit.textContent = change + '%';
+            changebit.style.color = colorchange;
+            pricebit.textContent = '$' + price;
+        }
+
+        else if (symb === 'eth' ) {
+
+            changeeth.textContent = change + '%';
+            changeeth.style.color = colorchange;
+            priceeth.textContent = '$' + j.price_usd;
+        }
+        else if (symb === 'bnb') {
+            changebnb.textContent = change + '%';
+            changebnb.style.color = colorchange;
+            pricebnb.textContent = '$' + price
+        }
+        else if (symb === 'usdt') {
+            changeusdt.textContent = change + '%';
+            changeusdt.style.color = colorchange;
+            priceusdt.textContent = '$' + price;
+        }
+
+    
+
+
+
+
+        document.querySelector("table tbody").innerHTML += `<tr><td><img src="https://assets.coincap.io/assets/icons/${symb}@2x.png" alt="">${coin}</td><td>$${price}</td><td style="color: ${colorchange};">${change}</td><td>${market}</td></tr>`
+
+
+    });
+}
+
+
+getPrice();
+
+$(function(){
+    if(screen.width<767){
+        $("nav a").click(function(e){
+            $(".navbar").slideUp();
+        })
+
+
+        $(".menu").click(function(){
+            $(".navbar").slideToggle();
+        });
+    }
+
+
+})
+
+const goUp=document.querySelector(".top");
+goUp.addEventListener("click",()=>{
+    window.scrollTo(0,0)
+})
